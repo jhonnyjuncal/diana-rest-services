414a8f245e34fd003f89cc3af95b3c457798c276
Víctor Panadero <vpherrer@gmail.com>
2020-12-09 22:31:57 +0100

    Primer comentario del proyecto

--- src/main/java/com/epicos/diana/Application.java
+++ src/main/java/com/epicos/diana/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	//IMPORTACION DEL PROYECTO COMPLETADO
+	//IMPORTACION DEL PROYECTO COMPLETADO 1er comentario
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
