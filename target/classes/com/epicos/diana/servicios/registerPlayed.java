2ac261a1a8a351f4650558b3e2bde99a64faacc2
Víctor Panadero <vpherrer@gmail.com>
2021-01-25 16:52:42 +0100

    Primera clase del registro de jugada

---
+++ target/classes/com/epicos/diana/servicios/registerPlayed.java
@@ -0,0 +1,13 @@
+package com.epicos.diana.servicios;
+
+import org.springframework.stereotype.Service;
+
+Public class Jugadores {
+    @Service
+    Public void Jugadores (String Nombre, short Jugada){
+
+        this.Nombre=Nombre;
+        this.Jugada=Jugada;
+
+    }
+}
